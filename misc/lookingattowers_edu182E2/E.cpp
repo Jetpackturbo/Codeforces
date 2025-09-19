86a245cfbd4bee208fff0a2d1a4bbe69133a8f90
Jack Li <jackpli@jlee.cs.washington.edu>
2025-09-19 16:43:37 -0700

    finish price tags

--- misc/lookingattowers_edu182E2/E.cpp
+++ misc/lookingattowers_edu182E2/E.cpp
@@ -36,6 +36,28 @@ typedef vector<string> vs;
 #define NMAX numeric_limits<ll>::max()
 
 const ll MOD = 998244353;
+struct mi {
+    int v; explicit operator int() const { return v; }
+    mi() { v = 0; }
+    mi(ll _v) :v(_v% MOD) { v += (v < 0) * MOD; }
+};
+mi& operator+=(mi& a, mi b) {
+    if ((a.v += b.v) >= MOD) a.v -= MOD;
+    return a;
+}
+mi& operator-=(mi& a, mi b) {
+    if ((a.v -= b.v) < 0) a.v += MOD;
+    return a;
+}
+mi operator+(mi a, mi b) { return a += b; }
+mi operator-(mi a, mi b) { return a -= b; }
+mi operator*(mi a, mi b) { return mi((ll)a.v * b.v); }
+mi& operator*=(mi& a, mi b) { return a = a * b; }
+mi pow(mi a, ll p) {
+    return p == 0 ? 1 : pow(a * a, p / 2) * (p & 1 ? a : 1);
+}
+mi inv(mi a) {  return pow(a, MOD - 2); }
+mi operator/(mi a, mi b) { return a * inv(b); }
 
 int main(){
     ifstream cin("F.in");
@@ -45,33 +67,45 @@ int main(){
         int n;
         cin >> n;
         vi nums(n);
-        vi lpeaks;
-        vi rpeaks;
+        vti lpeaks;
+        vti rpeaks;
         int ph = -1;
         for (int i = 0; i < n; i++) {
             cin >> nums[i];
-            if (nums[i] >= ph) {
+            if (nums[i] > ph) {
                 ph = nums[i];
-                lpeaks.push_back(i);
+                lpeaks.push_back(make_tuple(i,1));
+            }
+            else if (nums[i] == ph) {
+                lpeaks[lpeaks.size()-1] = make_tuple(ph, 1 + get<1>(lpeaks[lpeaks.size()-1]));
             }
         }
         int rph = -1;
         for (int i = n-1; i >= 0; i--) {
-            if (nums[i] >= rph) {
+            if (nums[i] > rph) {
                 rph = nums[i];
-                rpeaks.push_back(i);
-                if (rph == ph) {
-                    break;
-                }
+                rpeaks.push_back(make_tuple(i,1));
+            }
+            else if (nums[i] == rph) {
+                rpeaks[rpeaks.size()-1] = make_tuple(rph, 1 + get<1>(rpeaks[rpeaks.size()-1]));
+            }
+            if (nums[i] == ph) {
+                break;
             }
         }
         // note ph stores max height
-        ll lsum = 1;
+        mi lsum = 1;
+        mi rsum = 1;
         for (int i = 0; i < lpeaks.size() - 1; i++) {
+            // note first one should be zero
+            lsum *= pow(mi(2), get<0>(lpeaks[i+1]) - get<0>(lpeaks[i]) - 1);
+            if (get<1>(lpeaks[i]) > 0 ){
+                
+            }
 
         }
-
-        cout << (lsum * rsum) % mod << endl;
+        cout << lsum.v << " " << rsum.v << endl;
+        cout << (lsum * rsum).v << endl;
     }
     return 0;
 }
